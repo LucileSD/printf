f96c781994a7e8633bf0b61b93f6d5993dc9072a
Mustapha <elmustapha.abourar@gmail.com>
2022-03-12 14:46:00 +0100

    Putchar a character

---
+++ _char.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _char - print's a character.
+ * @c: charactere to print.
+ * 
+ * Return: always 1.
+ */
+int _char(va_list.choice)
+{
+    char ch = (char)va_arg(c, int)
+
+    _putchar(ch);
+    return(1);
+}
